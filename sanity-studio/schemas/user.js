8e744035bed3de4f0389eaaab23211df7dadba7d
KodywiththeK <jmsds.km@gmail.com>
2023-06-18 03:57:14 +0900

    data structure set-up

---
+++ sanity-studio/schemas/user.js
@@ -0,0 +1,63 @@
+export default {
+  name: 'user',
+  type: 'document',
+  title: 'User',
+  fields: [
+    {
+      title: 'Username',
+      name: 'username',
+      type: 'string',
+    },
+    {
+      title: 'Name',
+      name: 'name',
+      type: 'string',
+    },
+    {
+      title: 'Email',
+      name: 'email',
+      type: 'string',
+    },
+    {
+      title: 'Image',
+      name: 'iamge',
+      type: 'string',
+    },
+    {
+      title: 'Following',
+      name: 'following',
+      type: 'array',
+      of: [
+        {
+          type: 'reference', // 다른 사용자의 레퍼런스를 참고한다
+          to: [{type: 'user'}],
+        },
+      ],
+      validation: (Rule) => Rule.unique(), // 사용자는 고유해야한다.
+    },
+    {
+      title: 'Followers',
+      name: 'followers',
+      type: 'array',
+      of: [
+        {
+          type: 'reference',
+          to: [{type: 'user'}],
+        },
+      ],
+      validation: (Rule) => Rule.unique(),
+    },
+    {
+      title: 'Bookmarks',
+      name: 'bookmarks',
+      type: 'array',
+      of: [
+        {
+          type: 'reference',
+          to: [{type: 'post'}],
+        },
+      ],
+      validation: (Rule) => Rule.unique(),
+    },
+  ],
+}
